e88c0d44de09af109d5e0701bcb5180af0e71c1a
Michael Hagen <michaelhagen@nyu.edu>
2020-07-22 16:47:28 -0400

    Add EquipmentReservation handling

--- src/resources/reservation/reservation.controller.ts
+++ src/resources/reservation/reservation.controller.ts
@@ -1,6 +1,6 @@
 import { controllers, onResult } from "../../utils/crud";
 import { Request, Response } from "express";
-import pool, { inflate } from "../../utils/db";
+import pool, { error500, inflate } from "../../utils/db";
 
 const query = `
   SELECT
@@ -30,6 +30,53 @@ const query = `
     booking
 `;
 
+/**
+ * mysql does not accept key-value objects for bulk imports;
+ * values must be an array of arrays.
+ * Adjust the ordering here as needed.
+ * @param event Event object
+ */
+const flattenEquipment = (
+  equipment: {
+    [k: string]: string | number | boolean;
+  },
+  bookingId: number
+): (string | number | boolean)[] => [
+  equipment.id,
+  equipment.bookingId,
+  equipment.quantity,
+];
+
+const insertManyQuery = `
+  INSERT INTO equipment_reservation
+    (equipment_id, booking_id, quantity)
+  VALUES ?
+`;
+
+const reserveEquipment = (req: Request, res: Response) => {
+  pool.query(
+    "DELETE FROM equipment_reservation where booking_id = ?",
+    [req.body[0].bookingId],
+    onResult({ req, res }).create
+  );
+  pool.query(
+    insertManyQuery,
+    [
+      req.body.map((item) =>
+        flattenEquipment(item, req.body[0].bookingId as number)
+      ),
+    ],
+    (err) => {
+      const { context } = req.query;
+      if (err) return res.status(500).json(error500(err, context));
+      res.status(201).json({
+        data: "OK",
+        context,
+      });
+    }
+  );
+};
+
 export const getOne = (req: Request, res: Response) =>
   pool.query(
     query + "WHERE id = ?",
@@ -44,4 +91,5 @@ export default {
   ...controllers("booking", "id"),
   getOne,
   getMany,
+  reserveEquipment,
 };
